eb778515556198cd266011f4f1f932a051666efb
Ivan Fuligni <83448349+if-mindesk@users.noreply.github.com>
2024-07-24 16:17:04 +0200

    Add project files.

---
+++ TestRhino3dm/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TestRhino3dm
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TestRhino3dm/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Rhino.FileIO;
+
+namespace TestRhino3dm
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnPickRhino3dmFile(object sender, EventArgs e)
+        {
+            var customFileType = new FilePickerFileType(
+                new Dictionary<DevicePlatform, IEnumerable<string>>
+                {
+                    { DevicePlatform.iOS,  [] }, // UTType values
+                    { DevicePlatform.Android,  [] }, // MIME type
+                    { DevicePlatform.WinUI, new[] { "*.3dm" } }, // file extension
+                    { DevicePlatform.macOS,  [] }, // UTType values
+                });
+            PickOptions options = new()
+            {
+                PickerTitle = "Please select a Rhino3dm file",
+                FileTypes = customFileType,
+            };
+
+            var pickResult = await FilePicker.PickAsync(options);
+            if (pickResult is not null)
+            {
+                try
+                {
+                    var file3dm = File3dm.Read(pickResult.FullPath);
+                }
+                catch (Exception exception)
+                {
+                    if (Application.Current?.MainPage is not null)
+                    {
+                        await Application.Current.MainPage.DisplayAlert("Can' read 3dm file", exception.ToString(), "Ok");
+                    }
+                }
+            }
+        }
+    }
+}
